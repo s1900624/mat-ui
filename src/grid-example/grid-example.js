cbbf8e5b1310ecc8eeae6d2a81afa3f9e510f994
Your Name <you@example.com>
2019-11-24 17:51:17 +0200

    Update: grid-example

--- src/grid-example/grid-example.js
+++ src/grid-example/grid-example.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
 import './grid-example.css'
 
 const GridExample = () => {
-    return (
-        <div>
-            <Grid container spacing={1}>
-                <Grid container item xs={12} spacing={3}>
-                    TEST1
-                </Grid>
-                <Grid container item xs={12} spacing={3}>
-                    TEST2
-                </Grid>
-                <Grid container item xs={12} spacing={3}>
-                    TEST3
-                </Grid>
-            </Grid>
-        </div>
-    )
-}
+
+};
 
 export default GridExample;
