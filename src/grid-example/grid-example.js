91eff86fc7c332bcc0dd471dfce157fbe4e0a12a
Your Name <you@example.com>
2019-11-24 19:55:26 +0200

    Update grid component

--- src/grid-example/grid-example.js
+++ src/grid-example/grid-example.js
@@ -1,13 +1,64 @@
 import React from 'react';
+// import { Link } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import ButtonBase from '@material-ui/core/ButtonBase';
+import { Card, CardContent, CardMedia, Button, Typography, CardActions, Grid } from "@material-ui/core";
 import './grid-example.css'
 
-const GridExample = () => {
+import { datas } from "./data";
 
-};
+
+const useStyles = makeStyles({
+    gridContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    gridItem: {
+        padding: "1.0rem",
+        borderRadius: "1rem",
+        boxShadow: "-14px 20px 32px -10px rgba(0,0,0,0.75)",
+    },
+    cardMedia: {
+        height: "35vh",
+    },
+});
+
+const GridExample = () => {
+    const classes = useStyles();
+    return (
+        <main>
+            <Grid container className={classes.gridContainer} spacing={2}>
+                {datas.map((data) => {
+                    return(
+                        <Grid key={data.id} item xs={3} className={classes.gridItem}>
+                            <Card className={classes.gridItem}>
+                                <CardMedia
+                                    image={data.image}
+                                    component="img"
+                                    alt={data.alt}
+                                    height="300px" />
+                                <CardContent>
+                                    <Typography variant="h4">
+                                        {data.title}
+                                    </Typography>
+                                    <Typography variant="caption">
+                                        {data.text}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button
+                                        variant="contained"
+                                        color="secondary">
+                                        Click me
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    )
+                })}
+            </Grid>
+        </main>
+    )
+}
 
 export default GridExample;
