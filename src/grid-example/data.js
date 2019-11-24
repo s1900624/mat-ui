4281c90094b6827a74d8f0eeb4c205844f318a96
Your Name <you@example.com>
2019-11-24 19:55:59 +0200

    Add: new datas for view on the page

---
+++ src/grid-example/data.js
@@ -0,0 +1,23 @@
+export const datas = [
+    {
+        id: 1,
+        title: 'Is inquiry no he several 1',
+        image: 'https://images.freeimages.com/images/large-previews/70c/blue-heron-on-rock-1382247.jpg',
+        alt: '',
+        text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu'
+    },
+    {
+        id: 2,
+        title: 'Is inquiry no he several 2',
+        image: 'https://images.freeimages.com/images/large-previews/476/chicago-night-traffic-1447010.jpg',
+        alt: '',
+        text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu'
+    },
+    {
+        id: 3,
+        title: 'Is inquiry no he several 3',
+        image: 'https://images.freeimages.com/images/large-previews/53b/montenegro-sky-1370598.jpg',
+        alt: '',
+        text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu'
+    }
+];
