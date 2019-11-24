d54d2fff49c5a20f06306714a7c370e31e5767fb
Your Name <you@example.com>
2019-11-24 23:28:58 +0200

    Add code in contact page

--- src/app/app.js
+++ src/app/app.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AppNavbar from '../app-navbar';
 import Grid from '../grid-example';
+import Contact from '../contact';
 import './app.css';
 
 function App() {
   return (
     <div className="App">
       <Router basename="/">
-          <header className="App-header">
-            <AppNavbar/>
-            <Grid/>
-          </header>
+          <Switch>
+              <Route exact path="/">
+                  <header className="App-header">
+                      <AppNavbar/>
+                      <Grid/>
+                  </header>
+              </Route>
+              <Route path="/contact">
+                  <AppNavbar/>
+                  <Contact />
+              </Route>
+          </Switch>
       </Router>
     </div>
   );
---
+++ src/contact/contact.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { TextField, Grid, Button, Radio, RadioGroup,
+    FormControlLabel, FormControl, TextareaAutosize, FormLabel } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    gridContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: "130px",
+    },
+    form: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%"
+    },
+    input: {
+        marginBottom: "1rem"
+    },
+    textarea: {
+        marginBottom: "1rem",
+        width: "100%"
+    },
+    radiobuttons: {
+        float: "left",
+        width: "100%"
+    }
+});
+
+const Contact = () => {
+    const classes = useStyles();
+    return (
+        <Grid container className={classes.gridContainer} spacing={2}>
+            <Grid item xs={3}>
+                <form className={classes.form}>
+                    <FormControl className={classes.form}>
+                        <TextField className={classes.input} fullWidth label="Firstname" />
+                        <TextField className={classes.input} fullWidth label="Lastname" />
+                        <TextField className={classes.input} fullWidth label="Email" type="email" />
+                        <TextareaAutosize className={classes.textarea} rows={10}  fullWidth />
+                        <RadioGroup className={classes.radiobuttons} aria-label="gender" name="gender1">
+                            <FormControlLabel value="female" control={<Radio />} label="Female" />
+                            <FormControlLabel value="male" control={<Radio />} label="Male" />
+                            <FormControlLabel value="other" control={<Radio />} label="Other" />
+                        </RadioGroup>
+                        <FormLabel component="label" ></FormLabel>
+                        <Button variant="contained" color="primary">Send message</Button>
+                    </FormControl>
+                </form>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default Contact;
