393c8980fb3c2836140fedc36e1882512eb2cd5f
Your Name <you@example.com>
2019-11-27 16:47:02 +0200

    Updates

---
+++ precache-manifest.52217e2559a934b7d4128bbeab530a0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5758b20a3d7430a4d49896a96bf19615",
+    "url": "/mat-ui/index.html"
+  },
+  {
+    "revision": "858f6634c98dead2ac8b",
+    "url": "/mat-ui/static/css/main.5533c1a3.chunk.css"
+  },
+  {
+    "revision": "8c052683f27ee3666f0d",
+    "url": "/mat-ui/static/js/2.1073b7c5.chunk.js"
+  },
+  {
+    "revision": "858f6634c98dead2ac8b",
+    "url": "/mat-ui/static/js/main.e0af2a85.chunk.js"
+  },
+  {
+    "revision": "7d7a402c337a94bd73d4",
+    "url": "/mat-ui/static/js/runtime-main.160dfdc8.js"
+  }
+]);
